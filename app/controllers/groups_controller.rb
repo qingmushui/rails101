f9aa2687bc4b8c6fbc4abb5d5a7fced554d6dede
CloudM <qingmushui@163.com>
2017-01-03 16:25:34 +0800

    add calidation to title apply to edit/update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,8 +25,11 @@ end
 
  def update
    @group = Group.find(params[:id])
-   @group.update(group_params)
+   if @group.update(group_params)
    redirect_to groups_path,notice: "Update Success"
+  else
+    render :edit
+  end
  end
 
  def destroy
@@ -40,5 +43,4 @@ end
  def group_params
    params.require(:group).permit(:title, :description)
 end
-
 end
